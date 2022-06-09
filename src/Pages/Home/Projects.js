7c5aec7076c13c652dbb5844f365305d6ccadf82
SADEK HOSSAIN <shadeks16@gmail.com>
2022-06-09 17:11:13 +0600

    send message part done

--- src/Pages/Home/Projects.js
+++ src/Pages/Home/Projects.js
@@ -10,29 +10,29 @@ const Projects = () => {
         My Recent <span className="text-secondary">Projects</span>
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <div class="card max-w-xl bg-base-100 shadow-xl">
+        <div className="card max-w-xl bg-base-100 shadow-xl">
           <figure>
             <img
               src={img1}
               alt="Monota AutoParts"
             />
           </figure>
-          <div class="card-body bg-slate-200">
-            <h2 class="card-title">
+          <div className="card-body bg-slate-200">
+            <h2 className="card-title">
               Monota AutoParts
-              <div class="badge badge-secondary">Full Stack</div>
+              <div className="badge badge-secondary">Full Stack</div>
             </h2>
             <p className="text-justify">A Responsive, Fully-Functional Car Parts Manufacturer Website. Allowing Customers to Create an
 Account, Browse Products, Buy Products, Delete Products, Stripe Payment, & Write Reviews.</p>
-            <div class="card-actions justify-center font-semibold">
-              <div class="badge badge-outline">React</div>
-              <div class="badge badge-outline">Node.js</div>
-              <div class="badge badge-outline">Express.js</div>
-              <div class="badge badge-outline">MongoDB</div>
-              <div class="badge badge-outline">Tailwind</div>
-              <div class="badge badge-outline">MongoDB</div>
-              <div class="badge badge-outline">Firebase</div>
-              <div class="badge badge-outline">Heroku</div>
+            <div className="card-actions justify-center font-semibold">
+              <div className="badge badge-outline">React</div>
+              <div className="badge badge-outline">Node.js</div>
+              <div className="badge badge-outline">Express.js</div>
+              <div className="badge badge-outline">MongoDB</div>
+              <div className="badge badge-outline">Tailwind</div>
+              <div className="badge badge-outline">MongoDB</div>
+              <div className="badge badge-outline">Firebase</div>
+              <div className="badge badge-outline">Heroku</div>
             </div>
             <div className="text-center mt-4">
                 <button className="btn btn-success mr-5 w-32"><a href="https://monota-auto-parts.web.app/" target={'_blank'}>Live Site</a></button>
@@ -41,28 +41,28 @@ Account, Browse Products, Buy Products, Delete Products, Stripe Payment, & Write
           </div>
         </div>
 
-        <div class="card w-lg bg-base-100 shadow-xl">
+        <div className="card w-lg bg-base-100 shadow-xl">
           <figure>
             <img
               src={img2}
               alt="Car Manager"
             />
           </figure>
-          <div class="card-body bg-slate-200">
-            <h2 class="card-title">
+          <div className="card-body bg-slate-200">
+            <h2 className="card-title">
               Car Manager
-              <div class="badge badge-secondary">Full Stack</div>
+              <div className="badge badge-secondary">Full Stack</div>
             </h2>
             <p className="text-justify">Car Manager is a Warehouse Management Responsive Full-Stack Website. Users can Create an Account, Stock Update, Add Products, Delete Products, and Manage Products.</p>
-            <div class="card-actions justify-center font-semibold">
-              <div class="badge badge-outline">React</div>
-              <div class="badge badge-outline">Node.js</div>
-              <div class="badge badge-outline">Express.js</div>
-              <div class="badge badge-outline">MongoDB</div>
-              <div class="badge badge-outline">BootStrap</div>
-              <div class="badge badge-outline">MongoDB</div>
-              <div class="badge badge-outline">Firebase</div>
-              <div class="badge badge-outline">Heroku</div>
+            <div className="card-actions justify-center font-semibold">
+              <div className="badge badge-outline">React</div>
+              <div className="badge badge-outline">Node.js</div>
+              <div className="badge badge-outline">Express.js</div>
+              <div className="badge badge-outline">MongoDB</div>
+              <div className="badge badge-outline">BootStrap</div>
+              <div className="badge badge-outline">MongoDB</div>
+              <div className="badge badge-outline">Firebase</div>
+              <div className="badge badge-outline">Heroku</div>
             </div>
             <div className="text-center mt-4">
                 <button className="btn btn-success mr-5 w-32"><a href="https://car-manager-client.web.app/" target={'_blank'}>Live Site</a></button>
@@ -71,25 +71,25 @@ Account, Browse Products, Buy Products, Delete Products, Stripe Payment, & Write
           </div>
         </div>
 
-        <div class="card w-lg bg-base-100 shadow-xl">
+        <div className="card w-lg bg-base-100 shadow-xl">
           <figure>
             <img
               src={img3}
               alt="Denti Care"
             />
           </figure>
-          <div class="card-body bg-slate-200">
-            <h2 class="card-title">
+          <div className="card-body bg-slate-200">
+            <h2 className="card-title">
               Denti Care
-              <div class="badge badge-secondary">Front End</div>
+              <div className="badge badge-secondary">Front End</div>
             </h2>
             <p className="text-justify">Denti Care is a Dentist Website for a Single Doctor and He Provides Various Tooth Services. User Can Book any Service, Doctor Show Her Service, it is Static Website.</p>
-            <div class="card-actions justify-center font-semibold ">
-              <div class="badge badge-outline">React</div>
-              <div class="badge badge-outline">React BootStrap</div>
-              <div class="badge badge-outline">MongoDB</div>
-              <div class="badge badge-outline">Firebase</div>
-              <div class="badge badge-outline">Social Login</div>
+            <div className="card-actions justify-center font-semibold ">
+              <div className="badge badge-outline">React</div>
+              <div className="badge badge-outline">React BootStrap</div>
+              <div className="badge badge-outline">MongoDB</div>
+              <div className="badge badge-outline">Firebase</div>
+              <div className="badge badge-outline">Social Login</div>
             </div>
             <div className="text-center mt-4">
                 <button className="btn btn-success mr-5 w-32"><a href="https://react-denticare.web.app/" target={'_blank'}>Live Site</a></button>
--- src/Pages/Home/SendMessage.js
+++ src/Pages/Home/SendMessage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import emailjs from '@emailjs/browser';
 import contactPhoto from '../../assets/img/Call center-amico.png';
+import toast, { Toaster } from "react-hot-toast";
+
 
 const SendMessage = () => {
+  const sendEmail = (e) =>{
+    e.preventDefault();
+    emailjs.sendForm('service_bnzmv59', 'template_whdlavd', e.target, 'JkSZhoILpGOqjdGrw')
+      .then((result) => {
+          toast.success('Email Send Successfully');
+      }, (error) => {
+        toast.error('Email Send Not Successfully');
+      });
+      e.target.reset();
+  };
   return (
     <div className="px-5 md:px-28 mb-20">
       <h1 className="text-4xl text-center font-bold f-style pt-10 mb-16">
@@ -12,15 +25,19 @@ const SendMessage = () => {
             <img className="w-96" src={contactPhoto} alt="" />
         </div>
         <div>
-            <form  className="">
-            <input type="text" placeholder="Name" class="input input-bordered w-full max-w-md" />
-            <input type="text" placeholder="Email" class="mt-5 input input-bordered w-full max-w-md" />
-            <input type="text" placeholder="Subject" class="mt-5 input input-bordered w-full max-w-md" /> <br />
-            <textarea class=" mt-5 mb-5 textarea textarea-bordered w-full max-w-md" placeholder="Message"></textarea><br />
+            <form onSubmit={sendEmail}>
+            <input type="text" name="name" placeholder="Name" className="input input-bordered w-full max-w-md" required/>
+            <input type="email" name="email" placeholder="Email" className="mt-5 input input-bordered w-full max-w-md" required/>
+            <input type="text" name="subject" placeholder="Subject" className="mt-5 input input-bordered w-full max-w-md" required/> <br />
+            <textarea name="message" className=" mt-5 mb-5 textarea textarea-bordered w-full max-w-md" placeholder="Message" required></textarea><br />
             <input className="btn btn-primary btn-outline" type="submit" value="Send Message" />
             </form>
         </div>
       </div>
+      <Toaster
+  position="top-right"
+  reverseOrder={false}
+/>
     </div>
   );
 };
